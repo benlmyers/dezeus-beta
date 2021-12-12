fd63a6f9c66b59c4517dd09781f1c884f405fbc7
Ben Myers <benl.myers01@gmail.com>
2021-12-12 12:19:04 -0500

    🛠 Statement utility methods

--- src/Dezeus/Core/Statement.java
+++ src/Dezeus/Core/Statement.java
@@ -1,10 +1,34 @@
 package Dezeus.Core;
 
+import Dezeus.Logic.*;
+
 public abstract class Statement {
 
     public abstract Justification show(Deduction deduction) throws Invalidation;
 
     // Utility
 
-    
+    public And and(Statement b) {
+        return new And(this, b);
+    }
+
+    public Or or(Statement b) {
+        return new Or(this, b);
+    }
+
+    public Not not() {
+        return new Not(this);
+    }
+
+    public Cond provided(Statement ant) {
+        return new Cond(ant, this);
+    }
+
+    public Cond implies(Statement cons) {
+        return new Cond(this, cons);
+    }
+
+    public Bicond iff(Statement b) {
+        return new Bicond(this, b);
+    }
 }
